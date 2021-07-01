442f28d11b953dbd2cf0447089591c05ee6b5ce3
visor517 <visor517@yandex.ru>
2021-07-02 00:32:22 +0300

    added fail status

--- src/controllers/api/postRunBuild.js
+++ src/controllers/api/postRunBuild.js
@@ -3,6 +3,8 @@ const {postBase} = require('../shriBase')
 
 module.exports = async (req, res) => {
     const hash = req.params.commitHash
+
+    //console.log(hash)
     
     // здесь в клонированном репозитори будем искать по хешу commitMessage, branchName и authorName
 
@@ -13,13 +15,20 @@ module.exports = async (req, res) => {
         authorName: "string"
     }
 
-    const response = await postBase('build/request', params)
-    return res.json(response.data)
-    // здесь получим buildID
+    const request = await postBase('build/request', params) // получаем buildID
 
-    // запустим сборку и отправим запрос в /build/start
+    // запустим сборку
 
-    // тут сборка закончится и отправим запрос в /build/finish или /build/cancel (а может и нет)
+    const start = await postBase('build/start', {buildId: request.data.id, dateTime: new Date}) // отправляем запрос в /build/start
 
-    // а тут может что-нибудь отдадим на все это
+    // тут сборка закончится
+
+    const finish = await postBase('build/finish', { // отправляем запрос в /build/finish
+        buildId: request.data.id,
+        duration: Math.floor(Math.random() * 120),
+        success: Math.random() < 0.5,
+        buildLog: "string"
+    })
+
+    return {error: 0}   // тут может что-нибудь отдадим на все это    
 }
