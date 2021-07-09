02b8938431af8786d3318041bb54fcdd2985e908
visor517 <visor517@yandex.ru>
2021-07-09 21:49:56 +0300

    tests

--- hermione/test.hermione.js
+++ hermione/test.hermione.js
@@ -1,11 +1,12 @@
-describe('Конвертер', () => {
-    it('должен появится', async function() {
+describe('/build/1', () => {
+    it('должен показать коммит Test', async function() {
         const browser = this.browser
 
-        await browser.url('/')
-        //await browser.keys(['курс доллара к рублю','Enter'])
+        await browser.url('/build/1')
 
-        const converter = await browser.$('.commit')    //просто проверяет, что коммиты есть
-        await converter.waitForExist()
+        const name = await browser.$('.commit_name')   //просто проверяет, что коммит есть
+                // как получить содержимое тега что-то не нашел. getText не работает
+
+        await name.waitForExist()
     })
 })
--- tests/getSettings.test.js
+++
@@ -1,8 +0,0 @@
-// axios = require('axios')
-
-// const PORT = require('../src/index')
-
-// it('Handler get settings /api/settings', async () => {
-//     const data = await axios(`api/settings`)
-//     expect(data.data).toBeDefined()
-// })
